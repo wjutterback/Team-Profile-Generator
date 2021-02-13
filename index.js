ba71a74fdbd36f496d2db565333a242170f89462
Will Utterback <uwill08@gmail.com>
2021-02-12 23:57:53 -0600

    Corrected unused code in function init, refactored for try/catch, and consistent async method

--- index.js
+++ index.js
@@ -5,37 +5,35 @@ const inquirerQ = require('./assets/prompts');
 const fs = require('fs');
 
 //Prompts user for information about new manager and passes arrayed info to answerResults
-function init() {
+async function init() {
   const answerArr = [];
-  inquirer
-    .prompt(q.managerQuestions)
-    .then((data) => {
-      answerArr.push(data);
-      if (data.choice === 'Finish Building Team') {
-        makeHTML(answerArr);
-      } else {
-        answerResults(answerArr);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    let answerManager = await inquirerQ(q.managerQuestions);
+    answerArr.push(answerManager);
+    answerResults(answerArr);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 //Builds the rest of the team based on user choices, passes arrayed info to makeHTML which returns an HTML template
 async function answerResults(answerArr) {
-  let shunt = await inquirerQ(q.selectionShunt);
-  if (shunt.choice === 'Add an Engineer') {
-    let answerEngineer = await inquirerQ(q.engineerQuestions);
-    answerArr.push(answerEngineer);
-    answerResults(answerArr);
-  } else if (shunt.choice === 'Add an Intern') {
-    let answerIntern = await inquirerQ(q.internQuestions);
-    answerArr.push(answerIntern);
-    answerResults(answerArr);
-  } else if (shunt.choice === 'Finish Building Team') {
-    const templateData = await makeHTML(answerArr);
-    writeToFile(templateData);
+  try {
+    let shunt = await inquirerQ(q.selectionShunt);
+    if (shunt.choice === 'Add an Engineer') {
+      let answerEngineer = await inquirerQ(q.engineerQuestions);
+      answerArr.push(answerEngineer);
+      answerResults(answerArr);
+    } else if (shunt.choice === 'Add an Intern') {
+      let answerIntern = await inquirerQ(q.internQuestions);
+      answerArr.push(answerIntern);
+      answerResults(answerArr);
+    } else if (shunt.choice === 'Finish Building Team') {
+      const templateData = await makeHTML(answerArr);
+      writeToFile(templateData);
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
